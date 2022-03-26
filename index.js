3c67fe1d0cc4683568555a227ac6d5a7fc6ff8c6
Sammy Gallo <sammy.gallo@PH-0060.local>
2022-03-26 11:40:33 -0400

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,37 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+
+npc.walkNorth(500,()=> {
+    npc.walkWest(500)
+})
+
+
+/*
+Program a path for the npc that follows these directions and durations:
+
+North for 1400ms
+East for 1200ms
+South for 300ms
+East for 1500ms
+South for 1500ms
+West for 2700ms
+North for 400ms
+*/
+
+npc.walkNorth(1400, () => {
+    npc.walkEast(1200, () => {
+        npc.walkSouth(300, () => {
+            npc.walkEast(1500, () => {
+                npc.walkSouth(1500, () => {
+                    npc.walkWest(2700, () => {
+                        npc.walkNorth(400)
+                    })
+                })
+            })
+        })
+    })
+})
 
 // Create the inventory
 const inventory = newInventory()
